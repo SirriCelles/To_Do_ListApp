432158d39796aa508c7441918d172389c6f59f72
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-08-13 15:55:37 +0100

    initial setup with linters

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Webpack App',
             filename: 'index.html',
-            template: 'src/template.html'
+            template: 'src/index.html'
         }),
         new BundleAnalyzerPlugin(),
     ]
